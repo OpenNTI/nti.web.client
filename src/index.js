f1f3761d03a1d9a5681917373410a36acbf8dce6
Jonathan Grimes <jsg2021@gmail.com>
2021-04-16 18:17:05 -0500

    Add typedef

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ export * as User from './user';
 export { default as ExternalLibraryManager } from './ExternalLibraryManager';
 export * as TestUtils from './test-utils';
 
+/** @typedef {import('@nti/lib-interfaces/src/interface/DataServerInterface').default} Interface */
 /** @typedef {import('@nti/lib-interfaces/src/models/entities/User').default} User */
 /** @typedef {import('@nti/lib-interfaces/src/stores/Service').default} Service */
 /** @typedef {import('@nti/lib-interfaces/src/stores/UserPreferences').default} UserPreferences */
